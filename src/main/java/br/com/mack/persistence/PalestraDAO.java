7eae6a5bcf97eb4c13cd308d22be6fa2d43803ef
Yuri <iuri_novaes.2010@hotmail.com>
2017-05-03 21:08:12 +0000

    Mapeando

--- src/main/java/br/com/mack/persistence/PalestraDAO.java
+++ src/main/java/br/com/mack/persistence/PalestraDAO.java
@@ -152,9 +152,11 @@ public class PalestraDAO implements GenericDAO<Palestra> {
             
             ps.close();
             teste = keys.getInt(1);
+            System.out.println("************************************Passou pelo teste dentro do try")
         } catch (SQLException ex) {
             Logger.getLogger(PalestraDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
+        System.out.println("*************************************TESTEEEE" + teste);
         return teste;
     }
     
