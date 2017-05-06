da87c5e36c3466a16704fbfa56fd214a7a17ee55
Stella Fráguas <stella.fraguas@gmail.com>
2017-05-06 15:29:57 -0300

    Update PalestraDAO.java

--- src/main/java/br/com/mack/persistence/PalestraDAO.java
+++ src/main/java/br/com/mack/persistence/PalestraDAO.java
@@ -84,7 +84,7 @@ public class PalestraDAO implements GenericDAO<Palestra> {
                 palestra.setTema(rs.getString("tema"));
                 palestra.setCodigo(rs.getInt("codigo"));
                 palestra.setId_organizador(rs.getLong("id_organizador"));
-                palestras.add(palestra);
+                palestra.add(palestra);
             }
             ps.close();
 
